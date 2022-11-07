1e73396cbeda99b0a820bd9dc266c806c51f3e53
zcvfcat <zcvfcat@gmail.com>
2022-11-07 22:41:30 +0900

    chore (programmers score kit) greed problem migrate from python to js

--- 프로그래머스 고득점 kit/탐욕법/체육법.js
+++ 프로그래머스 고득점 kit/탐욕법/체육법.js
@@ -1,5 +1,20 @@
 /** @param {number} n @param {number[]} lost @param {number[]} reserve */
-function solution(n, lost, reserve) {}
+function solution(n, lost, reserve) {
+  const person = Array.from({ length: n + 1 }, () => 1)
+  lost.forEach((n) => (person[n] -= 1))
+  reserve.forEach((n) => (person[n] += 1))
+
+  for (let i = 1; i <= n; i++) {
+    if (person[i] === 2 && person[i - 1] === 0) {
+      person[i - 1] += 1
+      person[i] -= 1
+    } else if (person[i] === 2 && person[i + 1] === 0) {
+      person[i + 1] += 1
+      person[i] -= 1
+    }
+  }
+  return person.filter((p, i) => i !== 0 && p >= 1).length
+}
 console.log(solution(5, [2, 4], [1, 3, 5]) === 5)
 console.log(solution(5, [2, 4], [3]) === 4)
 console.log(solution(3, [3], [1]) === 2)
